e7ef3443d4d66ae513daf11d44a56ccc0ec6956b
MWizmo <masterwizmoo@yandex.ru>
2019-08-05 22:22:11 +0500

    v 5.0 Let's relise

---
+++ bot_token.py
@@ -0,0 +1,5 @@
+# token = '644032287:AAECDJ2hZJfNJnZcsuahPCoqyrx0lGQI6d8'  # main bot
+token = '573817226:AAHM6cFFyr64GS7c5ZWw6z_j6UHGNKQldBU' #KorpusBot (with avatar)
+# token = '691001400:AAF1zCuHnCpt3scH8_U1G3bNmMfbnLceW0g'
+# token = '977499970:AAH0RCPvTeurehgu9L_ThFBj0Ff66L-yRj4'  # InsteticaTestBot
+# 364905251
